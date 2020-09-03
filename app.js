923edb12e873a391ad3220d2d64897dbe8ed73fe
Arthur Ronconi <arthur.ronconi@tegrus.com.br>
2020-09-03 18:59:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const fetch = require('node-fetch')
+const apiKey = process.env.API_KEY
+
+const getData = (location) => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => console.log((data.main.temp - 273).toFixed(1)))
+    .catch(err => console.log(err))
+}
+
+getData('colatina')
+
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=YOUR_API_KEY
